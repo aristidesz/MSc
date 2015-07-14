ea7cd19ac3e6a44c4d5d5a1015da5e76b5a5cc9c
Aristides Zenonos <aristideszenonos@gmail.com>
2015-07-14 09:28:09 +0100

    Second

--- tomographic_db/urls.py
+++ tomographic_db/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls import patterns
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^image/$', views.image, name='image'),
+	url(r'^download/(?P<file_id>\d+)/?', 'views.download_file', 'myapp_download_file'),
 	url(r'^(?P<id_imageName>[0-9]+)/image/$', views.image, name='image'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- tomographic_db/views.py
+++ tomographic_db/views.py
@@ -52,21 +52,3 @@ def image(request):
 		template = loader.get_template('tomographic_db/home.html')
 		context = RequestContext(request,{'images_list': images_list,'new_TomoImages': new_TomoImages,})
 		return HttpResponse(template.render(context))
-
-def vote(request, imageName_id):
-    p = get_object_or_404(tomoImages, pk=imageName_id)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': p,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
