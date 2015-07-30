29f7a3aada595242d4dfef5c626670e6bac3ff24
Aristides Zenonos <aristideszenonos@gmail.com>
2015-07-31 00:31:23 +0100

    Fix CRLF

--- DeptSlices/draft2.py
+++
@@ -1,4 +0,0 @@
-bashCommand ="./dept-slices.gmt"
-import subprocess
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output = process.communicate()[0]
--- home/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class images(forms.Form):
-    imageNameForm = forms.CharField(label='ImageNameForm', max_length=100)
